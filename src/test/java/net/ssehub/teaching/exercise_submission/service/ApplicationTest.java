749b437a82a2ab8a26c4daab575457e37708192f
Adam <adam@sse.uni-hildesheim.de>
2023-01-23 14:45:08 +0100

    Initial commit: spring-boot project template

---
+++ src/test/java/net/ssehub/teaching/exercise_submission/service/ApplicationTest.java
@@ -0,0 +1,14 @@
+package net.ssehub.teaching.exercise_submission.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    
+    @Test
+    public void contextLoads() {
+        
+    }
+    
+}
