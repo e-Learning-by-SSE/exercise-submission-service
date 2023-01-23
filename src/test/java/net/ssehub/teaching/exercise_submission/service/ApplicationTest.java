cf1518f8b07cbfdc5fc7e0fd8016d9e6d1c5db37
Adam <adam@sse.uni-hildesheim.de>
2023-01-23 18:24:53 +0100

    ApplicationTest: create empty teststorage directory

--- src/test/java/net/ssehub/teaching/exercise_submission/service/ApplicationTest.java
+++ src/test/java/net/ssehub/teaching/exercise_submission/service/ApplicationTest.java
@@ -1,10 +1,20 @@
 package net.ssehub.teaching.exercise_submission.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApplicationTest {
+    
+    @BeforeAll
+    public static void createEmptyTeststorage() throws IOException {
+        Files.createDirectory(Path.of("teststorage"));
+    }
     
     @Test
     public void contextLoads() {
