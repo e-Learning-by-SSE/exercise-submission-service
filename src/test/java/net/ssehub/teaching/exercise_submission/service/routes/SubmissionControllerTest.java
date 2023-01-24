9f6761c6de91f11321b95025291812ca23a77648
Adam <adam@sse.uni-hildesheim.de>
2023-01-24 13:40:54 +0100

    Refactor SubmissionControllerTest

--- src/test/java/net/ssehub/teaching/exercise_submission/service/routes/SubmissionControllerTest.java
+++ src/test/java/net/ssehub/teaching/exercise_submission/service/routes/SubmissionControllerTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,70 +25,63 @@ import net.ssehub.teaching.exercise_submission.service.submission.checks.ResultM
 
 public class SubmissionControllerTest {
 
-    @Test
-    public void submissionAccepted() throws StorageException {
-        String course = "java-sose23";
-        String assignment = "Homework03";
-        String group = "JP042";
-        
-        List<FileDto> files = List.of(
+    private SubmissionTarget target;
+    
+    private List<FileDto> files;
+    
+    private SubmissionManager acceptingManager;
+    private SubmissionResultDto acceptingResult;
+    
+    private SubmissionManager rejectingManager;
+    private SubmissionResultDto rejectingResult;
+    
+    @BeforeEach
+    public void initSubmission() throws StorageException {
+        target = new SubmissionTarget("java-sose23", "Homework03", "JP042");
+        files = List.of(
             new FileDto("Main.java", "testcontent"),
             new FileDto("util/Uitl.java", "testcontent")
         );
         
-//        SubmissionBuilder sb = new SubmissionBuilder(group);
-//        sb.addUtf8File(Path.of("Main.java"), "testcontent");
-//        sb.addUtf8File(Path.of("util/Uitl.java"), "testcontent");
-//        Submission submission = sb.build();
-        
-        SubmissionManager manager = mock(SubmissionManager.class);
-        SubmissionResultDto managerResult = new SubmissionResultDto();
-        managerResult.setAccepted(true);
-        managerResult.setMessages(List.of(new CheckMessageDto("test", MessageType.WARNING, "mock")));
-        when(manager.submit(eq(new SubmissionTarget(course, assignment, group)), any()))
+        acceptingManager = mock(SubmissionManager.class);
+        acceptingResult = new SubmissionResultDto();
+        acceptingResult.setAccepted(true);
+        acceptingResult.setMessages(List.of(new CheckMessageDto("test", MessageType.WARNING, "mock")));
+        when(acceptingManager.submit(eq(target), any()))
             // TODO: actually match submission above
-                .thenReturn(managerResult);
+                .thenReturn(acceptingResult);
         
-        SubmissionController controller = new SubmissionController(manager);
+        rejectingManager = mock(SubmissionManager.class);
+        rejectingResult = new SubmissionResultDto();
+        rejectingResult.setAccepted(false);
+        rejectingResult.setMessages(List.of(new CheckMessageDto("test", MessageType.WARNING, "mock")));
+        when(rejectingManager.submit(eq(target), any()))
+            // TODO: actually match submission above
+                .thenReturn(rejectingResult);
+    }
+    
+    @Test
+    public void submissionAccepted() throws StorageException {
+        SubmissionController controller = new SubmissionController(acceptingManager);
         
-        ResponseEntity<SubmissionResultDto> result = controller.submit(course, assignment, group, files);
+        ResponseEntity<SubmissionResultDto> result = controller.submit(
+                target.course(), target.assignmentName(), target.groupName(), files);
         
         assertAll(
-            () -> assertSame(managerResult, result.getBody()),
+            () -> assertSame(acceptingResult, result.getBody()),
             () -> assertEquals(HttpStatus.CREATED, result.getStatusCode())
         );
     }
     
     @Test
     public void submissionRejected() throws StorageException {
-        String course = "java-sose23";
-        String assignment = "Homework03";
-        String group = "JP042";
+        SubmissionController controller = new SubmissionController(rejectingManager);
         
-        List<FileDto> files = List.of(
-            new FileDto("Main.java", "testcontent"),
-            new FileDto("util/Uitl.java", "testcontent")
-        );
-        
-//        SubmissionBuilder sb = new SubmissionBuilder(group);
-//        sb.addUtf8File(Path.of("Main.java"), "testcontent");
-//        sb.addUtf8File(Path.of("util/Uitl.java"), "testcontent");
-//        Submission submission = sb.build();
-        
-        SubmissionManager manager = mock(SubmissionManager.class);
-        SubmissionResultDto managerResult = new SubmissionResultDto();
-        managerResult.setAccepted(false);
-        managerResult.setMessages(List.of(new CheckMessageDto("test", MessageType.WARNING, "mock")));
-        when(manager.submit(eq(new SubmissionTarget(course, assignment, group)), any()))
-            // TODO: actually match submission above
-                .thenReturn(managerResult);
-        
-        SubmissionController controller = new SubmissionController(manager);
-        
-        ResponseEntity<SubmissionResultDto> result = controller.submit(course, assignment, group, files);
+        ResponseEntity<SubmissionResultDto> result = controller.submit(
+                target.course(), target.assignmentName(), target.groupName(), files);
         
         assertAll(
-            () -> assertSame(managerResult, result.getBody()),
+            () -> assertSame(rejectingResult, result.getBody()),
             () -> assertEquals(HttpStatus.OK, result.getStatusCode())
         );
     }
